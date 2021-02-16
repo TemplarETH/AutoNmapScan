c4949cbbc42ccf1a32313a5be4f85071509f563a
TemplarETH <templar.eth@gmail.com>
2021-02-15 20:40:15 -0500

    Primera prueba con indetificador de SO

--- ans.py
+++ ans.py
@@ -1,3 +1,28 @@
-import sys
 import nmap3
+import sys
 
+
+
+def main():
+	print("Automatizador de nmap")
+	nmap = nmap3.Nmap()
+	so = nmap.nmap_os_detection("192.168.20.32")
+	#print (so)
+	dic_so = so['192.168.20.32']
+	#print(dic_so)
+	name_so = dic_so['osmatch']
+	if name_so == []:
+		print("No se encontro información")
+	else:
+		print(name_so)	
+		#name_so = name_so[0]
+		for i in name_so:
+			name_so = i['name']
+			print(name_so)
+
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt():
+		sys.exit()
